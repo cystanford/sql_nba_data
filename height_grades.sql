7617e0490acfc391ed86055ba02dbbf48f1231d3
cystanford <chenyang@wucai.com>
2019-04-16 18:23:56 +0800

    NBA球员数据表

---
+++ height_grades.sql
@@ -0,0 +1,4 @@
+INSERT INTO `height_grades` VALUES ('A', 2.00, 2.50);
+INSERT INTO `height_grades` VALUES ('B', 1.90, 1.99);
+INSERT INTO `height_grades` VALUES ('C', 1.80, 1.89);
+INSERT INTO `height_grades` VALUES ('D', 1.60, 1.79);
---
+++ team.sql
@@ -0,0 +1,3 @@
+INSERT INTO `team` VALUES (1001, '底特律活塞');
+INSERT INTO `team` VALUES (1002, '印第安纳步行者');
+INSERT INTO `team` VALUES (1003, '亚特兰大老鹰');
